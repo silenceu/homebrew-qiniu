e848b80a0b7b7c970c7cb946579e62315e9ce5cf
silence <szyhs24546@gmail.com>
2020-03-18 18:08:52 +0800

    add qrsctl formula

---
+++ Formula/qrsctl@3.0.20180423.rb
@@ -0,0 +1,17 @@
+class QrsctlAT3020180423 < Formula
+    desc "A simple command-line auxiliary tool implemented based on Qiniu Cloud Storage API"
+    homepage "https://developer.qiniu.com/kodo/tools/1300/qrsctl"
+    url "https://raw.githubusercontent.com/silenceu/homebrew-qiniu/tree/master/archive/qrsctl-v3.0.20180423"
+    sha256 "0c4793aea74d40e785a388377943f9e28bfb58bd0d9bb701daf59899b61cd223"
+
+    bottle :unneeded
+
+    def install
+        system "mv qrsctl-v3.0.20180423 qrsctl"
+    	bin.install "qrsctl"
+    end
+
+    test do
+        system "#{bin}/qrsctl"
+    end
+end
